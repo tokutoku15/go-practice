fff8213b9a4ae49daca856f476ebea4a9ef14cf7
tokutoku15 <kazu.you.715@gmail.com>
2021-08-29 01:28:17 +0900

    2章完了

--- chap1/chat/client.go
+++ chap1/chat/client.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -9,16 +12,22 @@ type client struct {
 	// socketはこのクライアントのためのWebsocket
 	socket *websocket.Conn
 	// sendはメッセージが送られるチャネル
-	send chan []byte
+	send chan *message
 	// roomはこのクライアントが参加しているルーム
 	room *room
+	// userDataはユーザーに関する情報を保持する
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
 	for {
-		if _, msg, err := c.socket.ReadMessage(); err == nil {
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
 			c.room.forward <- msg
 		} else {
+			log.Fatal("read error:", err)
 			break
 		}
 	}
@@ -27,7 +36,7 @@ func (c *client) read() {
 
 func (c *client) write() {
 	for msg := range c.send {
-		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+		if err := c.socket.WriteJSON(msg); err != nil {
 			break
 		}
 	}
