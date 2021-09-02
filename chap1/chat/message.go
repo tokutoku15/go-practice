cc00bb8d4752903889b86c7cf7a2fb017da761a2
tokutoku15 <kazu.you.715@gmail.com>
2021-09-03 01:43:30 +0900

    bootstrapでchat.htmlの整形

--- chap1/chat/message.go
+++ chap1/chat/message.go
@@ -4,7 +4,8 @@ import "time"
 
 // messageは1つのメッセージを表す
 type message struct {
-	Name    string
-	Message string
-	When    time.Time
+	Name      string
+	Message   string
+	When      time.Time
+	AvatarURL string
 }
